f692fde471bfde05554c7fcb9d3ff55a7ea74dd5
deny <deniansyah@erxhealth.co.uk>
2023-09-05 08:39:44 +0700

    .

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\EmployeeApiController;
 
 use App\Http\Controllers\ProductApiController;
 use App\Http\Controllers\CustomerApiController;
@@ -21,9 +22,17 @@ use App\Http\Controllers\AttendanceController;
 //api/logout
 //api/refresh
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
 });
 
 Route::post('/attendance/check-in', [AttendanceController::class, 'checkIn']);
+Route::prefix('employee')->middleware('auth:sanctum')->group(function () {
+    Route::get('', [EmployeeApiController::class, 'index']);
+    Route::post('', [EmployeeApiController::class, 'store']);
+    Route::get('{id}', [EmployeeApiController::class, 'show']);
+    Route::put('{id}', [EmployeeApiController::class, 'update']);
+    Route::delete('{id}', [EmployeeApiController::class, 'destroy']);
+});
